2392d27a9dc296ff4c402a66e15aef9e8343448f
so-hee bae <123538321+bae-sohee@users.noreply.github.com>
2023-06-30 11:42:24 +0900

    distance fixed

--- test.py
+++ test.py
@@ -1,165 +1,4 @@
-# test.py 다중분류
-import torch
-
-from sklearn.metrics import roc_auc_score
-from sklearn.manifold import TSNE
-import matplotlib.pyplot as plt
-import numpy as np
-
-num_classes = 15
-
-def softmax(scores):
-    if scores.ndim == 1:
-        scores = scores.reshape(1, -1)  # Reshape to (1, num_classes)
-    exp_scores = np.exp(scores)
-    probabilities = exp_scores / np.sum(exp_scores, axis=1, keepdims=True)
-    return probabilities
-
-def eval(net, c, dataloader, device):
-    """Testing the Deep SVDD model"""
-
-    scores = []
-    encoded_data = []
-    labels = []
-    correct_predictions = 0
-    total_predictions = 0
-    class_correct = [0] * num_classes
-    class_total = [0] * num_classes
-
-    net.eval()
-    print('Testing...')
-
-    with torch.no_grad():
-        for x, y in dataloader:
-            x = x.float().to(device)
-            y = y.to(device) # Label
-            z = net(x) # output : z_dim 32
-
-            score = torch.sum((z - c) ** 2, dim=1)
-
-            scores.append(score.detach().cpu())
-
-            encoded_data.append(z.detach().cpu().numpy())
-            labels.append(y.detach().cpu().numpy())
-
-            _, predicted_labels = torch.max(score, dim=0)
-
-            correct_predictions += (predicted_labels == y).sum().item()
-            total_predictions += y.size(0)
-
-            for i in range(len(y)):
-                label = y[i].item()
-                class_correct[label] += int(predicted_labels.item() == label)
-                class_total[label] += 1
-
-    encoded_data = np.concatenate(encoded_data, axis=0)
-    labels = np.concatenate(labels, axis=0)
-    c = c.cpu().numpy()  # Move c tensor from GPU to CPU and convert to numpy array
-    c = np.expand_dims(c, axis=0)  # Add an extra dimension to match the dimensions of encoded_data
-    encoded_data = np.append(encoded_data, c, axis=0) # Append the value of c to encoded_data
-
-    # t-SNE 적용
-    
-    tsne = TSNE(n_components=2)
-    print("encoded_data", encoded_data)
-    encoded_data_2d = tsne.fit_transform(encoded_data)  # 32->2 dimension
-    print("encoded_data_2d", encoded_data_2d)
-
-    # Extract the last appended value c
-    center_2d = encoded_data_2d[-1]
-    print("c", center_2d)
-    encoded_data_2d = encoded_data_2d[:-1]  # Remove the last appended value c
-
-    print("labels",labels)
-
-    labels = labels.astype(np.int64)
-
-    # Calculate distance percentiles
-    distances = np.linalg.norm(encoded_data_2d - center_2d, axis=1)
-    # percentiles = np.percentile(distances, [25, 50, 75])
-    
-    # 시각화
-    plt.figure(figsize=(10, 8))
-    class_names = [
-        'bottle', 'cable', 'capsule', 'carpet', 'grid', 'hazelnut',
-        'leather', 'metal_nut', 'pill', 'screw', 'tile', 'toothbrush',
-        'transistor', 'wood', 'zipper']
-    colors = ['red', 'orange', 'yellow', 'green', 'blue', 'purple', 'pink',
-              'brown', 'black', 'cyan', 'grey', 'lime', 'navy', 'salmon', 'turquoise']
-    
-    indices = []
-    class_labels = []
-    for i in range(len(class_names)):
-        class_label = i
-        class_indices = np.where(labels == class_label)[0]
-        indices.append(class_indices)
-        class_labels.append(class_label)
-        plt.scatter(encoded_data_2d[class_indices, 0], encoded_data_2d[class_indices, 1],
-                    color=colors[i], label=class_names[i], s=1, alpha=0.5)
-        
-    # # Plot center and boundaries based on t-SNE coordinates
-    # plt.scatter(center_2d[0].item(), center_2d[1].item(), color='green', marker='o', label='Center', s=3)
-    # for percentile in percentiles:
-    #     boundary_radius = (center_2d[0] * (percentile / 100)).item()
-    #     boundary_circle = plt.Circle(center_2d, boundary_radius, color='purple', fill=False)
-    #     plt.gca().add_patch(boundary_circle)
-
-    plt.xlabel('t-SNE Dimension 1')
-    plt.ylabel('t-SNE Dimension 2')
-    plt.title('t-SNE Visualization')
-
-    # plt.legend()
-    handles, c_labels = plt.gca().get_legend_handles_labels()
-    by_label = dict(zip(c_labels, handles))
-    plt.legend(by_label.values(), by_label.keys(), bbox_to_anchor=(1.02, 1), loc='upper left')
-    plt.show()
-
-    print('class_indices:',indices)
-    print('class_label:',class_labels)
-    print('labels:',labels)
-
-    
-    scores = torch.cat(scores, dim=0)
-    scores = scores.cpu().numpy()  # Move scores tensor from GPU to CPU and convert to numpy arrayscores = torch.cat(scores, dim=0)
-    # Apply softmax to convert scores to probabilities
-    probabilities = softmax(scores)
-
-    print('Probabilities Shape:', probabilities.shape)
-    print('Probabilities:', probabilities)
-
-    predicted_labels = np.argmax(probabilities, axis=1)
-
-    print('Labels Size:', labels.shape)
-    print('Predicted Labels Size:', predicted_labels.shape)
-
-    #accuracy
-    accuracy = correct_predictions / total_predictions * 100
-    print('Accuracy: {:.2f}%'.format(accuracy))
-
-    class_accuracy = [class_correct[i] / class_total[i] * 100 for i in range(num_classes)]
-    print('Class-wise Accuracy:', class_accuracy)
-
-    # Reshape scores array if necessary
-    if scores.ndim == 1:
-        scores = scores.reshape(-1, 1)  # Reshape to (num_samples, 1)
-
-    # Convert scores to probabilities using softmax
-    probabilities = softmax(scores)
-    
-    from sklearn.preprocessing import LabelBinarizer
-
-    lb = LabelBinarizer()
-    labels = lb.fit_transform(labels)
-    
-    # ROC AUC 점수 계산
-    auc_score1 = roc_auc_score(labels, probabilities, multi_class='ovr', average='macro') * 100
-    auc_score2 = roc_auc_score(labels, probabilities, multi_class='ovo', average='macro') * 100
-    print('ROC AUC score (ovr): {:.2f}'.format(auc_score1))
-    print('ROC AUC score (ovo): {:.2f}'.format(auc_score2))
-    
-    return labels, scores, accuracy, class_accuracy
-
-# # test.py 이진분류
+# # test.py 다중분류
 # import torch
 
 # from sklearn.metrics import roc_auc_score
@@ -167,15 +6,25 @@ def eval(net, c, dataloader, device):
 # import matplotlib.pyplot as plt
 # import numpy as np
 
+# num_classes = 15
+
+# def softmax(scores):
+#     if scores.ndim == 1:
+#         scores = scores.reshape(1, -1)  # Reshape to (1, num_classes)
+#     exp_scores = np.exp(scores)
+#     probabilities = exp_scores / np.sum(exp_scores, axis=1, keepdims=True)
+#     return probabilities
+
 # def eval(net, c, dataloader, device):
 #     """Testing the Deep SVDD model"""
 
 #     scores = []
-#     labels = []
 #     encoded_data = []
-#     true_labels = []
+#     labels = []
 #     correct_predictions = 0
 #     total_predictions = 0
+#     class_correct = [0] * num_classes
+#     class_total = [0] * num_classes
 
 #     net.eval()
 #     print('Testing...')
@@ -189,17 +38,22 @@ def eval(net, c, dataloader, device):
 #             score = torch.sum((z - c) ** 2, dim=1)
 
 #             scores.append(score.detach().cpu())
-#             labels.append(y.cpu())
 
 #             encoded_data.append(z.detach().cpu().numpy())
-#             true_labels.append(y.detach().cpu().numpy())
+#             labels.append(y.detach().cpu().numpy())
 
-#             predicted_labels = torch.round(score).long()
+#             _, predicted_labels = torch.max(score, dim=0)
+
 #             correct_predictions += (predicted_labels == y).sum().item()
 #             total_predictions += y.size(0)
 
+#             for i in range(len(y)):
+#                 label = y[i].item()
+#                 class_correct[label] += int(predicted_labels.item() == label)
+#                 class_total[label] += 1
+
 #     encoded_data = np.concatenate(encoded_data, axis=0)
-#     true_labels = np.concatenate(true_labels, axis=0)
+#     labels = np.concatenate(labels, axis=0)
 #     c = c.cpu().numpy()  # Move c tensor from GPU to CPU and convert to numpy array
 #     c = np.expand_dims(c, axis=0)  # Add an extra dimension to match the dimensions of encoded_data
 #     encoded_data = np.append(encoded_data, c, axis=0) # Append the value of c to encoded_data
@@ -215,36 +69,196 @@ def eval(net, c, dataloader, device):
 #     center_2d = encoded_data_2d[-1]
 #     print("c", center_2d)
 #     encoded_data_2d = encoded_data_2d[:-1]  # Remove the last appended value c
-    
+
 #     print("labels",labels)
-#     print("true_labels", true_labels)
+
+#     labels = labels.astype(np.int64)
 
 #     # Calculate distance percentiles
 #     distances = np.linalg.norm(encoded_data_2d - center_2d, axis=1)
-#     percentiles = np.percentile(distances, [25, 50, 75])
-
+#     # percentiles = np.percentile(distances, [25, 50, 75])
+    
 #     # 시각화
-
-#     # Plot t-SNE visualization with color-coded labels # 0: label은 normal, 1: label은 anomaly
-#     plt.scatter(encoded_data_2d[true_labels == 0, 0], encoded_data_2d[true_labels == 0, 1], color='blue', label='Normal', s=1, alpha=0.5)
-#     plt.scatter(encoded_data_2d[true_labels == 1, 0], encoded_data_2d[true_labels == 1, 1], color='red', label='Anomaly', s=1, alpha=0.5)
-
-#     # Plot center and boundaries based on t-SNE coordinates
-#     plt.scatter(center_2d[0].item(), center_2d[1].item(), color='green', marker='o', label='Center', s=3)
-#     for percentile in percentiles:
-#         boundary_radius = (center_2d[0] * (percentile / 100)).item()
-#         boundary_circle = plt.Circle(center_2d, boundary_radius, color='purple', fill=False)
-#         plt.gca().add_patch(boundary_circle)
+#     plt.figure(figsize=(10, 8))
+#     class_names = [
+#         'bottle', 'cable', 'capsule', 'carpet', 'grid', 'hazelnut',
+#         'leather', 'metal_nut', 'pill', 'screw', 'tile', 'toothbrush',
+#         'transistor', 'wood', 'zipper']
+#     colors = ['red', 'orange', 'yellow', 'green', 'blue', 'purple', 'pink',
+#               'brown', 'black', 'cyan', 'grey', 'lime', 'navy', 'salmon', 'turquoise']
+    
+#     indices = []
+#     class_labels = []
+#     for i in range(len(class_names)):
+#         class_label = i
+#         class_indices = np.where(labels == class_label)[0]
+#         indices.append(class_indices)
+#         class_labels.append(class_label)
+#         plt.scatter(encoded_data_2d[class_indices, 0], encoded_data_2d[class_indices, 1],
+#                     color=colors[i], label=class_names[i], s=1, alpha=0.5)
+        
+#     # # Plot center and boundaries based on t-SNE coordinates
+#     # plt.scatter(center_2d[0].item(), center_2d[1].item(), color='green', marker='o', label='Center', s=3)
+#     # for percentile in percentiles:
+#     #     boundary_radius = (center_2d[0] * (percentile / 100)).item()
+#     #     boundary_circle = plt.Circle(center_2d, boundary_radius, color='purple', fill=False)
+#     #     plt.gca().add_patch(boundary_circle)
 
 #     plt.xlabel('t-SNE Dimension 1')
 #     plt.ylabel('t-SNE Dimension 2')
 #     plt.title('t-SNE Visualization')
-#     plt.legend()
+
+#     # plt.legend()
+#     handles, c_labels = plt.gca().get_legend_handles_labels()
+#     by_label = dict(zip(c_labels, handles))
+#     plt.legend(by_label.values(), by_label.keys(), bbox_to_anchor=(1.02, 1), loc='upper left')
 #     plt.show()
 
+#     print('class_indices:',indices)
+#     print('class_label:',class_labels)
+#     print('labels:',labels)
+
+    
+#     scores = torch.cat(scores, dim=0)
+#     scores = scores.cpu().numpy()  # Move scores tensor from GPU to CPU and convert to numpy arrayscores = torch.cat(scores, dim=0)
+#     # Apply softmax to convert scores to probabilities
+#     probabilities = softmax(scores)
+
+#     print('Probabilities Shape:', probabilities.shape)
+#     print('Probabilities:', probabilities)
+
+#     predicted_labels = np.argmax(probabilities, axis=1)
+
+#     print('Labels Size:', labels.shape)
+#     print('Predicted Labels Size:', predicted_labels.shape)
+
+#     #accuracy
 #     accuracy = correct_predictions / total_predictions * 100
-#     labels, scores = torch.cat(labels).numpy(), torch.cat(scores).numpy()
-#     print('ROC AUC score: {:.2f}'.format(roc_auc_score(labels, scores)*100))
 #     print('Accuracy: {:.2f}%'.format(accuracy))
 
-#     return labels, scores
+#     class_accuracy = [class_correct[i] / class_total[i] * 100 for i in range(num_classes)]
+#     print('Class-wise Accuracy:', class_accuracy)
+
+#     # Reshape scores array if necessary
+#     if scores.ndim == 1:
+#         scores = scores.reshape(-1, 1)  # Reshape to (num_samples, 1)
+
+#     # Convert scores to probabilities using softmax
+#     probabilities = softmax(scores)
+    
+#     from sklearn.preprocessing import LabelBinarizer
+
+#     lb = LabelBinarizer()
+#     labels = lb.fit_transform(labels)
+    
+#     # ROC AUC 점수 계산
+#     auc_score1 = roc_auc_score(labels, probabilities, multi_class='ovr', average='macro') * 100
+#     auc_score2 = roc_auc_score(labels, probabilities, multi_class='ovo', average='macro') * 100
+#     print('ROC AUC score (ovr): {:.2f}'.format(auc_score1))
+#     print('ROC AUC score (ovo): {:.2f}'.format(auc_score2))
+    
+#     return labels, scores, accuracy, class_accuracy
+
+# test.py 이진분류
+import torch
+
+from sklearn.metrics import roc_auc_score
+from sklearn.manifold import TSNE
+import matplotlib.pyplot as plt
+import numpy as np
+
+def eval(net, c, dataloader, device):
+    """Testing the Deep SVDD model"""
+
+    scores = []
+    labels = []
+    encoded_data = []
+    true_labels = []
+    correct_predictions = 0
+    total_predictions = 0
+
+    net.eval()
+    print('Testing...')
+
+    with torch.no_grad():
+        for x, y in dataloader:
+            x = x.float().to(device)
+            y = y.to(device) # Label
+            z = net(x) # output : z_dim 32
+
+            score = torch.sum((z - c) ** 2, dim=1)
+
+            scores.append(score.detach().cpu())
+            labels.append(y.cpu())
+
+            encoded_data.append(z.detach().cpu().numpy())
+            true_labels.append(y.detach().cpu().numpy())
+
+            predicted_labels = torch.round(score).long()
+            correct_predictions += (predicted_labels == y).sum().item()
+            total_predictions += y.size(0)
+
+        # 전체 epoch 종료 후 모든 데이터 포인트와 중심 c 간의 거리 계산. 유클리드 거리
+        with torch.no_grad():
+            distances = torch.norm(net(x) - c, dim=1)
+            min_distance = torch.min(distances).item()
+            avg_distance = torch.mean(distances).item()
+            max_distance = torch.max(distances).item()
+
+        
+        print('distances:{:.3f}',distances)
+        print('Min Distance_test: {:.3f}'.format(min_distance))
+        print('Avg Distance_test: {:.3f}'.format(avg_distance))
+        print('Max Distance_test: {:.3f}'.format(max_distance))
+
+    encoded_data = np.concatenate(encoded_data, axis=0)
+    true_labels = np.concatenate(true_labels, axis=0)
+    c = c.cpu().numpy()  # Move c tensor from GPU to CPU and convert to numpy array
+    c = np.expand_dims(c, axis=0)  # Add an extra dimension to match the dimensions of encoded_data
+    encoded_data = np.append(encoded_data, c, axis=0) # Append the value of c to encoded_data
+
+    # t-SNE 적용
+    
+    tsne = TSNE(n_components=2)
+    # print("encoded_data", encoded_data)
+    encoded_data_2d = tsne.fit_transform(encoded_data)  # 32->2 dimension
+    # print("encoded_data_2d", encoded_data_2d)
+
+    # Extract the last appended value c
+    center_2d = encoded_data_2d[-1]
+    # print("c", center_2d)
+    encoded_data_2d = encoded_data_2d[:-1]  # Remove the last appended value c
+    
+    # print("labels",labels)
+    # print("true_labels", true_labels)
+
+    # Calculate distance percentiles
+    distances = np.linalg.norm(encoded_data_2d - center_2d, axis=1)
+    percentiles = np.percentile(distances, [25, 50, 75])
+
+    # 시각화
+
+    # Plot t-SNE visualization with color-coded labels # 0: label은 normal, 1: label은 anomaly
+    plt.scatter(encoded_data_2d[true_labels == 0, 0], encoded_data_2d[true_labels == 0, 1], color='blue', label='Normal', s=1, alpha=0.5)
+    plt.scatter(encoded_data_2d[true_labels == 1, 0], encoded_data_2d[true_labels == 1, 1], color='red', label='Anomaly', s=1, alpha=0.5)
+
+    # Plot center and boundaries based on t-SNE coordinates
+    plt.scatter(center_2d[0].item(), center_2d[1].item(), color='green', marker='x', label='Center', s=5)
+    
+    # for percentile in percentiles:
+    #     boundary_radius = (center_2d[0] * (percentile / 100)).item()
+    #     boundary_circle = plt.Circle(center_2d, boundary_radius, color='purple', fill=False)
+    #     plt.gca().add_patch(boundary_circle)
+
+    plt.xlabel('t-SNE Dimension 1')
+    plt.ylabel('t-SNE Dimension 2')
+    plt.title('t-SNE Visualization')
+    plt.legend()
+    plt.show()
+
+    accuracy = correct_predictions / total_predictions * 100
+    labels, scores = torch.cat(labels).numpy(), torch.cat(scores).numpy()
+    print('ROC AUC score: {:.2f}'.format(roc_auc_score(labels, scores)*100))
+    print('Accuracy: {:.2f}%'.format(accuracy))
+
+    return labels, scores
--- train.py
+++ train.py
@@ -8,6 +8,10 @@ from barbar import Bar
 from model import autoencoder, network
 from utils.utils import weights_init_normal
 
+from sklearn.manifold import TSNE
+from sklearn.metrics import pairwise_distances
+import matplotlib.pyplot as plt
+from matplotlib.patches import Circle
 
 class TrainerDeepSVDD:
     def __init__(self, args, data, device):
@@ -87,10 +91,11 @@ class TrainerDeepSVDD:
                                weight_decay=self.args.weight_decay)
         scheduler = optim.lr_scheduler.MultiStepLR(optimizer, 
                     milestones=self.args.lr_milestones, gamma=0.1)
-
+        
         net.train()
+ 
         for epoch in range(self.args.num_epochs):
-            total_loss = 0
+            total_loss = 0  # 각 미니배치에서의 총 손실
             for x, _ in Bar(self.train_loader):
                 x = x.float().to(self.device)
 
@@ -101,12 +106,106 @@ class TrainerDeepSVDD:
                 optimizer.step()
 
                 total_loss += loss.item()
+        
             scheduler.step()
             print('Training Deep SVDD... Epoch: {}, Loss: {:.3f}'.format(
-                   epoch, total_loss/len(self.train_loader))) 
+                    epoch, total_loss/len(self.train_loader))) 
+            
+        # 전체 epoch 종료 후 모든 데이터 포인트와 중심 c 간의 거리 계산. 유클리드 거리
+        with torch.no_grad():
+            distances = torch.norm(net(x) - c, dim=1)
+            min_distance = torch.min(distances).item()
+            avg_distance = torch.mean(distances).item()
+            max_distance = torch.max(distances).item()
+
+        print('distances:{:.3f}',distances)
+        print('Min Distance_train: {:.3f}'.format(min_distance))
+        print('Avg Distance_train: {:.3f}'.format(avg_distance))
+        print('Max Distance_train: {:.3f}'.format(max_distance))
+
         self.net = net
-        self.c = c       
+        self.c = c
 
+        # net.train()
+        # for epoch in range(self.args.num_epochs):
+        #     total_loss = 0
+        #     for x, _ in Bar(self.train_loader):
+        #         x = x.float().to(self.device)
 
+        #         optimizer.zero_grad()
+        #         z = net(x)
+        #         loss = torch.mean(torch.sum((z - c) ** 2, dim=1))
+        #         loss.backward()
+        #         optimizer.step()
 
+        #         total_loss += loss.item()
 
+                
+        #     scheduler.step()
+        #     print('Training Deep SVDD... Epoch: {}, Loss: {:.3f}'.format(
+        #            epoch, total_loss/len(self.train_loader))) 
+        # self.net = net
+        # self.c = c       
+
+         # visualization
+        self.visualize_embeddings()
+
+    # visualization
+    def visualize_embeddings(self):
+        self.net.eval()
+        embeddings = []
+        targets = []
+        with torch.no_grad():
+            for x, y in self.train_loader:
+                x = x.float().to(self.device)
+                z = self.net(x)
+                embeddings.append(z.cpu().numpy())
+                targets.append(y.numpy())
+        embeddings = np.concatenate(embeddings, axis=0)
+        targets = np.concatenate(targets, axis=0)
+
+        # Apply t-SNE to reduce embeddings to 2D
+        tsne = TSNE(n_components=2, random_state=42)
+        embeddings_tsne = tsne.fit_transform(embeddings)
+
+        # Plot the embeddings with reduced marker size
+        plt.figure(figsize=(8, 8))
+        plt.scatter(embeddings_tsne[:, 0], embeddings_tsne[:, 1], c=targets, cmap='viridis', s=2)
+        plt.colorbar(label='Class')
+
+        # Set the axes limits
+        plt.xlim(embeddings_tsne[:, 0].min() - 1, embeddings_tsne[:, 0].max() + 1)
+        plt.ylim(embeddings_tsne[:, 1].min() - 1, embeddings_tsne[:, 1].max() + 1)
+
+        # Plot the center point
+        c = self.c.cpu().numpy()
+        plt.scatter(c[0], c[1], c='red', marker='x', label='Center')
+
+        # # Calculate pairwise distances and average distance to c  마지막 epoch의 distance
+        # distances = np.linalg.norm(embeddings - c, axis=1)
+        # avg_distance = np.mean(distances)
+        # min_distance = np.min(distances)
+        # max_distance = np.max(distances)
+
+        # # Draw circle with max distance as radius
+        # circle = Circle((c[0], c[1]), max_distance, color='red', fill=True, linestyle='dashed', label='Max Distance')
+        # plt.gca().add_patch(circle)
+
+        # # Draw circle with avg distance as radius
+        # circle2 = Circle((c[0], c[1]), avg_distance, color='orange', fill=True, linestyle='dashed', label='Avg Distance')
+        # plt.gca().add_patch(circle2)
+
+        plt.title('Embeddings Visualization (t-SNE)')
+        plt.xlabel('Dimension 1')
+        plt.ylabel('Dimension 2')
+
+        # # Display distance information
+        # distance_info = f"Average distance: {avg_distance:.3f}\nMin distance: {min_distance:.3f}\nMax distance: {max_distance:.3f}"
+        # plt.text(0.05, 0.95, distance_info, transform=plt.gca().transAxes, va='top', bbox=dict(boxstyle='round', facecolor='white', alpha=0.5))
+
+        plt.legend()
+        plt.show()
+
+        # print('Average distance to c:', avg_distance)
+        # print('Minimum distance to c:', min_distance)
+        # print('Maximum distance to c:', max_distance)
